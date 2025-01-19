fc6328d4b3d19159a4b091af9559653711100502
ARUN L <108601293+Arun100203@users.noreply.github.com>
2025-01-19 03:09:27 +0000

    added host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "e-commerce-production-1762.up.railway.app"
 end
